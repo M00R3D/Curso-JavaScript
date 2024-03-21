f1a590d8d82a9cc862729d2c9f184b8802482876
job moore <jobmurdan@ohtmail.com>
2024-03-21 14:57:14 -0700

    cree el script numeros.js que nos ayudara a comprender los tipos de numeros y sus funciones y constructores

---
+++ numeros.js
@@ -0,0 +1,7 @@
+let a = 6
+let b = new Number(5)//al crearlo con constructor pasa algo similar al string que nos puede decir mas propiedades en la consola
+let c = 6.552
+
+console.log(a,b);
+console.log(c.toFixed(2));//el metodo to fixed nos dara el numero decimal con el numero de decimales que 
+console.log(c.toFixed(6));//le pongamos en el parametro
