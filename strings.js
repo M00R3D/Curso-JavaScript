a77e4334f0178434efdf67351d83c39829c3baf1
job moore <jobmurdan@ohtmail.com>
2024-03-21 13:00:51 -0700

    he creado el script strings.js que nos comienza a dar una idea de los metodos y el constructor a la hora de crear variables de tipo string

---
+++ strings.js
@@ -0,0 +1,7 @@
+let nombre = "Jason"
+let apellido ="AnDerSon"
+let hola = new String("hola mundo")
+console.log(hola," ",nombre,apellido);
+//al haber creado la variable hola, con un constructor oficial, en lugar de unicamente desginarle su tipo de valor 
+//por lo debilmente tipado del lenguaje, esta se divide como una cadena de texto y en la consola puedo ver las posiciones de cada
+//una de los caracteres que conforman el string
