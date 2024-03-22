15b7a4c2a9cde960174ea666ee0b8f1ec4d2d54a
job moore <jobmurdan@ohtmail.com>
2024-03-22 14:29:20 -0700

    cree el script indefinidos.js que nos ayuda a comprender el valor null, undefined y NaN, ademas agregue notas al archivo de notas.txt

---
+++ indefinidos.js
@@ -0,0 +1,11 @@
+//undefined es lo que nos sale cuando no le damos ningun valor al declarar una variable
+let indefinida
+console.log(indefinida)
+
+//null es un valor asignable a una variable para que no contenga nada
+let nula = null
+console.log(nula)
+
+//NaN(Not a Number) nos sale al querer operar numeros con otros tipos de datos
+let noNumerica="no soy un numero" / 2
+console.log(noNumerica);
