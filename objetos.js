284963e214b1437d2746d334dafceed4f98a1fbb
job moore <jobmurdan@ohtmail.com>
2024-03-28 20:54:05 -0700

    añadimos mas ejemplos a la parte de objetos y creamos el script operadores que nos ayuda a comprender mas como opera en distintas formas este lenguaje

--- objetos.js
+++ objetos.js
@@ -14,8 +14,16 @@ const humano=
         celular:6134872047
     },
     saludo:function() {
-        console.log("hola")
+        console.log(`hola, mi nombre es ${this.nombre} y tengo ${this.edad} años`)
     }
 }
 
 console.log(humano)
+console.log(humano.apellido)
+console.log(humano.edad)
+console.log(humano.soltero)
+console.log(humano.contaco.celular)
+humano.saludo()
+console.log(Object.keys(humano))//Metodo del prototipo object que nos regresa el nombre de cada propiedad
+console.log(Object.values(humano))//Metodo del prototipo object que nos regresa cada uno de los valores internos
+console.log(humano.hasOwnProperty("nombre"))//nos regresa si un objeto contiene dicha propiedad
---
+++ operadores.js
@@ -0,0 +1,59 @@
+// Operadores aritméticos
+// -------------------------------------------
+let a = 10;
+let b = 5;
+
+let suma = a + b;
+console.log("Suma:", suma);
+
+let resta = a - b;
+console.log("Resta:", resta);
+
+let multiplicacion = a * b;
+console.log("Multiplicación:", multiplicacion);
+
+let division = a / b;
+console.log("División:", division);
+
+let modulo = a % b;
+console.log("Módulo:", modulo);
+
+let incremento = ++a;
+console.log("Incremento:", incremento);
+
+let decremento = --b;
+console.log("Decremento:", decremento);
+// Comparadores de Relación
+// -------------------------------------------
+let x = 10;
+let y = 5;
+
+let igualdad = x == y;
+console.log("Igualdad:", igualdad);
+
+let mayorQue = x > y;
+console.log("Mayor que:", mayorQue);
+
+let menorQue = x < y;
+console.log("Menor que:", menorQue);
+
+let mayorOIgualQue = x >= y;
+console.log("Mayor o igual que:", mayorOIgualQue);
+
+let menorOIgualQue = x <= y;
+console.log("Menor o igual que:", menorOIgualQue);
+
+// -------------------------------------------
+// "=" significa asignacion, "==" significa igualdad de valores ,
+//            "==="significa igualdad de tipo de variable y valor
+let asignacion = 7
+// -------------------------------------------
+console.log(7==7)
+console.log("7"==7)
+console.log(7==7)
+console.log(0==false)
+// -------------------------------------------
+console.log(7===7)
+console.log("7"===7)
+console.log(7===7)
+console.log(0===false)
