176ca312f02adb7a74b4f96cf510b26f98805f58
job moore <jobmurdan@ohtmail.com>
2024-04-02 15:36:29 -0700

    he añadido un ejemplo de foreach, en conjunto con parametros rest para decir todos los elementos de un objeto u arreglo y otro ejemplo para usarlo en la concatenacion de arreglos de forma facil

--- restSpread.js
+++ restSpread.js
@@ -1,9 +1,26 @@
 function sumar(a,b,...c) ///si ponemos ... significa que declaramos un parametro
 {               ///un parametro del tipo rest
-    let resultado=a+b
+    let resultado=(a+b)
     c.forEach(function(n) {
-        resultado+=c
+        resultado+=n
     })
 
     return resultado
 }
+console.log(sumar(1,2))
+console.log(sumar(1,2,3))
+console.log(sumar(1,2,3,4))
+console.log(sumar(1,2,3,4,5))
+console.log(sumar(1,2,3,4,5,6))
+console.log(sumar(1,2,3,4,5,6,7))
+
+arregloA=[1,2,3,4,5]
+console.log("arregloA",arregloA)
+arregloB=[6,7,8,9,0]
+console.log("arregloB",arregloB)
+arregloMalHecho=[arregloA,arregloB]
+console.log("arregloMalHecho",arregloMalHecho)
+arregloAb=[...arregloA,...arregloB]//como podemos 
+//observar podemos usar la notacion rest para concatenar
+//  por elementos y no por objeto o arreglo
+console.log("arregloAb",arregloAb)
