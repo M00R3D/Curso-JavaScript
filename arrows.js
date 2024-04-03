922312e3f49c8ac8f2e5e3959642401ac3565f49
job moore <jobmurdan@ohtmail.com>
2024-04-03 13:55:23 -0700

    termine de añadir ejemplos de arrow function y explicar sus propiedades

--- arrows.js
+++ arrows.js
@@ -1,9 +1,48 @@
-const funcionSuma = function(a,b,c)
+// const funcionSuma = function(a,b,c)
+// {
+//     console.log(a+b+c)
+// }
+
+// const flechaSuma = (a,b,c)=>console.log(a+b+c)//Este tipo de funciones nos ayuda a reducir el tamaño
+//de la sintaxis de cada funcion que creamos
+
+// funcionSuma(1,0,1)
+// flechaSuma(1,2,3)
+
+
+
+// const sumar = function(a,b)
+// {
+//     return a+b
+// }
+
+const sumar = (a,b) => a+b
+console.log("arrow function de suma  sumar(1,2):",sumar(1,2))
+
+
+const numeros = [1,2,3,4,5]
+
+// numeros.forEach(function(elemento,posicion)
+// {
+//     console.log(`Elemento:${elemento}   Posicion:${posicion}`)
+// }              )
+
+numeros.forEach((elemento,posicion)=>
+console.log(`Elemento:${elemento}   Posicion:${posicion}`))
+
+
+const objetoA=
 {
-    console.log(a+b+c)
+    nombre:"Objeto"
+    ,
+    funcionBloque:function()
+        {
+        console.log(this)
+        }
+    ,
+    funcionFlecha : () =>console.log(this)
 }
-
-const flechaSuma = (a,b,c)=>console.log(a+b+c)
-
-funcionSuma(1,0,1)
-flechaSuma(1,2,3)
+objetoA.funcionBloque()
+objetoA.funcionFlecha()///al declarar una arrow function dentro de un objeto, esta se creara realmente
+//dentro del objeto window, ya que al no tener llaves, no respeta la logica de bloques
+//Similar a crear un var(Arrow) en lugar de un let(Funcion Clasica)
