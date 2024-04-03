750a963d123b9df1be151710e37f55d1c22573b9
job moore <jobmurdan@ohtmail.com>
2024-04-03 15:51:45 -0700

    añadi la parte de herencia de prototipos en el script prototipos

--- prototipos.js
+++ prototipos.js
@@ -9,14 +9,32 @@ function Humano(nombre,edad)
     //     // console.log("Hola Mundo");
     // }//esto consumiria mucho espacio al crear cada funcion en cada objeto tipo humano
 }
-Humano.prototype.saludar = function()
+Humano.prototype.saludar = function()//asi declaramos la funcion de forma que no gaste tanta memoria
     {
         console.log(`Hola, soy ${this.nombre} y tengo ${this.edad} años`);
         // console.log("Hola Mundo");
     }
 
+function Programador(nombre,edad,lenguaje)
+{
+    this.super=Humano//definir super como el prototipo padre
+    this.super(nombre,edad)//heredar parametros con super
+    this.lenguaje=lenguaje//parametros propios de este hijo
+}
+
+Programador.prototype=Humano//herencia
+Programador.prototype.constructor=Programador//declarar constructor propio
+Programador.prototype.mencionarLenguaje=function() {//crear funcion en hijo
+    console.log(`mi lenguaje favorito para programar es: ${this.lenguaje}`)
+}
+Programador.prototype.saludar=Humano.prototype.saludar///pasar funciones de padre a hijo
 const carlos=new Humano("Carlos",30),ivan=new Humano("Ivan",20)
 console.log(carlos)
 console.log(ivan)
 carlos.saludar()
 ivan.saludar()
+
+const job = new Programador("Job",20,"Lua")
+console.log(job)
+job.saludar()
+job.mencionarLenguaje()
