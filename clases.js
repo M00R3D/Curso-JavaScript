7c6d0d2d67a28504afb1913e33e439f355ff4557
job moore <jobmurdan@ohtmail.com>
2024-04-05 16:33:40 -0700

    he añadido los getters, setters y metodos estaticos al script clases .js

--- clases.js
+++ clases.js
@@ -7,12 +7,10 @@ class Item
         this.peso=peso
         this.cantidad=cantidad
     }
-
     caracteristicas()
     {
         console.log(`precio=${this.precio},tamanio=${this.tamanio},peso=${this.peso},cantidad=${this.cantidad}`)
     }
-
     sumarPrecio(suma)
     {
         this.precio+=suma
@@ -26,13 +24,18 @@ class Tecnologia extends Item
     {
         super(precio,tamanio,peso,cantidad)
         this.tipo=tipo
+        this.paradigma=null
     }
 
     actualizar()
     {
         console.log("Este dispositivo se ha actualizado")
     }
+    
+    get getParadigma(){return this.paradigma}
+    set setParadigma(x){this.paradigma=x}
 
+    static metodoEstatico(){return "este metodo se puede usar sin crear un objeto"}
 }
 
 const celular= new Item(5000,"grande",1.025,1)
@@ -42,3 +45,11 @@ celular.sumarPrecio(2000)
 const mouse = new Tecnologia(2000,"chico",1.203,2,"periferico")
 mouse.actualizar()
 mouse.caracteristicas()
+console.log(mouse.getParadigma)
+mouse.setParadigma="Movimiento"
+console.log(mouse.getParadigma)
+console.log(Tecnologia.metodoEstatico())
+console.log(mouse.metodoEstatico())
+// Los metodos estaticos pueden ser utilizados unicamente sin crear el objeto 
+// al que pertenecen, y los getters y setters funcionan bastante similar a otros
+// lenguajes solo que en vez de administrarse como funciones son administrados como propiedades
