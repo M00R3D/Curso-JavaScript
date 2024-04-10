1d5705df8a0b9b3ff5923ceef05d6d3c1edd0f9e
job moore <jobmurdan@ohtmail.com>
2024-04-10 14:48:26 -0700

    termine los primeros ejercicios

--- modulos.js
+++ modulos.js
@@ -1,5 +1,5 @@
 //modulos.js
-// import {ingreso} from "./entrada.js"; ///a pesar de que solo estamos tratando de importar ingreso, al importar cualquier script se ejecutara todo el codigo dentro de ella, por lo cual es 
+import {ingreso} from "./entrada.js"; ///a pesar de que solo estamos tratando de importar ingreso, al importar cualquier script se ejecutara todo el codigo dentro de ella, por lo cual es 
 //mas conveniente realizar nuestros metodos dentro de sus propios objetos
 import {ObjetoEjecutable} from "./funcionesAutoejecutables.js"
 
---
+++ primerosEjercicios.js
@@ -0,0 +1,29 @@
+                            // 1) Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10.
+                            // 2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
+                            // 3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
+                            // 4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
+
+
+
+// const numChar= (x)=> new String(x).length
+// let cadena = window.prompt("introduzca la cadena de texto:")
+// console.log(numChar(cadena))
+
+
+// const recortarTexto=(texto,longitud)=>new String(texto).slice(0,longitud)
+// let texto= window.prompt("introduzca el texto:")
+// let longitud= window.prompt("introduzca la longitud:")
+// console.log(recortarTexto(texto,longitud))
+
+
+// const arregloTexto=(texto,caracter)=>new String(texto).split(caracter)
+// let texto= window.prompt("introduzca el texto:")
+// let caracter= window.prompt("introduzca el caracter:")
+// console.log(arregloTexto(texto,caracter))
+
+
+// const repetirTexto=(texto,numero)=>
+// {    for(let i=0;i<numero;i++){console.log(texto)} }
+// let texto= window.prompt("introduzca el texto:")
+// let num= window.prompt("introduzca el numero:")
+// repetirTexto(texto,num)
