1d849e12351eb83fa6c6bd502c905aa5e4a2e811
job moore <jobmurdan@ohtmail.com>
2024-04-17 16:04:10 -0700

    termine el ultimo ejercicio de logica

--- novenosEjercicios.js
+++ novenosEjercicios.js
@@ -1,7 +1,7 @@
-// La clase recibirá un objeto al momento de instanciarse con los siguentes datos
+                // La clase recibirá un objeto al momento de instanciarse con los siguentes datos
                 // : id de la película en IMDB, titulo, director, año de estreno, país o países de origen, géneros y calificación en IMBD.
                 //   - Todos los datos del objeto son obligatorios.
-                //   - Valida que el id IMDB tenga 9 caracteres, los primeros 2 sean letras y los 
+                s//   - Valida que el id IMDB tenga 9 caracteres, los primeros 2 sean letras y los 
                 //      7 restantes números.
                 //   - Valida que el título no rebase los 100 caracteres.
                 //   - Valida que el director no rebase los 50 caracteres.
@@ -11,12 +11,12 @@
                 //   - Valida que los géneros introducidos esten dentro de los géneros 
                 //      aceptados*.
                 //   - Crea un método estático que devuelva los géneros aceptados*.
-//   - Valida que la calificación sea un número entre 0 y 10 pudiendo ser 
-//     decimal de una posición.
-//   - Crea un método que devuelva toda la ficha técnica de la película.
-//   - Apartir de un arreglo con la información de 3 películas genera 3 
-//     instancias de la clase de forma automatizada e imprime la ficha técnica 
-//     de cada película.    
+                //   - Valida que la calificación sea un número entre 0 y 10 pudiendo ser 
+                //     decimal de una posición.
+                //   - Crea un método que devuelva toda la ficha técnica de la película.
+                //   - Apartir de un arreglo con la información de 3 películas genera 3 
+                //     instancias de la clase de forma automatizada e imprime la ficha técnica 
+                //     de cada película.    
 
 
 class Pelicula
@@ -44,8 +44,56 @@ class Pelicula
         if (typeof clasificacion === "number" && clasificacion >= 0 && clasificacion <= 10){this.clasificacion = parseFloat(clasificacion.toFixed(1))}
         else{this.clasificacion = "indefinido"}
     }
-
-    static obtenerGenerosAceptados() {
-        return ["acción", "aventura", "comedia", "drama", "terror", "ciencia ficción", "romance", "documental"]
+    static obtenerGenerosAceptados() {return ["acción", "aventura", "comedia", "drama", "terror", "ciencia ficción", "romance", "documental"]}
+    ficha(){
+    return `imdb:${this.imdb}, titulo:${this.titulo}, director:${this.director}, anio:${this.anio}, pais:${this.pais}, genero:${this.genero}, clasificacion:${this.clasificacion}`
     }
+    static generarPeliculasDesdeArreglo= function(arrPeliculas) {
+        const peliculas = [];
+        for (const pelicula of arrPeliculas) {
+            peliculas.push(new Pelicula(
+                pelicula.imdb,pelicula.titulo,pelicula.director,pelicula.anio
+                ,pelicula.pais,pelicula.genero,pelicula.clasificacion
+            ));
+        }
+        return peliculas;
+    }
+}
+
+
+const informacionPeliculas = [
+    { 
+        imdb: "tt0111161",
+        titulo: "The Shawshank Redemption",
+        director: "Frank Darabont",
+        anio: 1994,
+        pais: ["Estados Unidos"],
+        genero: ["drama"],
+        clasificacion: 9.3
+    },
+    { 
+        imdb: "tt0068646",
+        titulo: "The Godfather",
+        director: "Francis Ford Coppola",
+        anio: 1972,
+        pais: ["Estados Unidos"],
+        genero: ["crimen", "drama"],
+        clasificacion: 9.2
+    },
+    { 
+        imdb: "tt0071562",
+        titulo: "The Godfather: Part II",
+        director: "Francis Ford Coppola",
+        anio: 1974,
+        pais: ["Estados Unidos"],
+        genero: ["crimen", "drama"],
+        clasificacion: 9.0
+    }
+]
+
+
+const peliculas = Pelicula.generarPeliculasDesdeArreglo(informacionPeliculas)
+
+for (const a of peliculas) {
+    console.log(a.ficha())
 }
